e266a0a10a419943b442d6656e40007d28ca43dc
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-10-24 12:55:24 -0300

    preparingAmbientForMySql_JDBC

---
+++ CourseJava/src/JDBC/db/DB.java
@@ -0,0 +1,50 @@
+package JDBC.db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection conn = null;
+
+    // Responsavel por conectar com o banco
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    // Responsavel por fechar a conexão com o banco
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    // Responsavel por carregar as propriedades do banco, no arquivo db.properties
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+}
---
+++ CourseJava/src/JDBC/source/Program.java
@@ -0,0 +1,14 @@
+package JDBC.source;
+
+import JDBC.db.DB;
+
+import java.sql.Connection;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+
+    }
+}
