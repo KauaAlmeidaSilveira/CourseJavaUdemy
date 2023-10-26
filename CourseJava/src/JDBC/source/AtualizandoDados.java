deaa065b1907547ae26f0e479e2897a1ba19e077
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-10-26 17:55:45 -0300

    updatingData

---
+++ CourseJava/src/JDBC/source/AtualizandoDados.java
@@ -0,0 +1,36 @@
+package JDBC.source;
+
+import JDBC.db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AtualizandoDados {
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "UPDATE seller " +
+                        "SET BaseSalary = BaseSalary + ? " +
+                        "WHERE DepartmentId = ?");
+
+            st.setDouble(1, 3000.0);
+            st.setInt(2, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows Affected: " + rowsAffected);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+}
