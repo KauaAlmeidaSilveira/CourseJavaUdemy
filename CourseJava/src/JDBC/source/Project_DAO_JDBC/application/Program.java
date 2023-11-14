a04e076103ee194c8098f04dba3ce66adb7ffcc8
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-11-14 09:29:19 -0300

    findByDepartment

--- CourseJava/src/JDBC/source/Project_DAO_JDBC/application/Program.java
+++ CourseJava/src/JDBC/source/Project_DAO_JDBC/application/Program.java
@@ -2,14 +2,25 @@ package JDBC.source.Project_DAO_JDBC.application;
 
 import JDBC.source.Project_DAO_JDBC.model.dao.DaoFactory;
 import JDBC.source.Project_DAO_JDBC.model.dao.SellerDao;
+import JDBC.source.Project_DAO_JDBC.model.entities.Department;
 import JDBC.source.Project_DAO_JDBC.model.entities.Seller;
+
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
-        Seller seller = sellerDao.findById(3);
-        System.out.println(seller);
+        Seller seller01 = sellerDao.findById(3);
+        System.out.println(seller01);
+
+
+        System.out.println("=== TEST 2: seller findByDepartment ===");
+        Department dep = new Department(2, null);
+        List<Seller> sellerList = sellerDao.findByDepartment(dep.getId());
+        sellerList.forEach(System.out::println);
+
+
     }
 }
