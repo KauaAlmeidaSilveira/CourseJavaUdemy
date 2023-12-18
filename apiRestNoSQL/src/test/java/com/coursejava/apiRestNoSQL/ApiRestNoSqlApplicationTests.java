3860faa959a9a21ecc72a5119aa9514ecc86f105
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-12-17 21:26:26 -0300

    Project Created

---
+++ apiRestNoSQL/src/test/java/com/coursejava/apiRestNoSQL/ApiRestNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursejava.apiRestNoSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
