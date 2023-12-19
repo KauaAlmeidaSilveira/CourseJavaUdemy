965633bd8f3fd68c9a459f94d79149594662b360
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-12-19 17:19:05 -0300

    Query with many criteria

--- apiRestNoSQL/src/main/java/com/coursejava/apiRestNoSQL/services/PostService.java
+++ apiRestNoSQL/src/main/java/com/coursejava/apiRestNoSQL/services/PostService.java
@@ -6,6 +6,7 @@ import com.coursejava.apiRestNoSQL.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,5 +32,10 @@ public class PostService {
     //Using @Query
     public List<Post> searchTitle(String text){
         return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 *60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
