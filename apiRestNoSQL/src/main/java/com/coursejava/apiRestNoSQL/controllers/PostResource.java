98a69f1fed920dc4288f6e18d0ecb774c25eea79
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-12-19 09:18:59 -0300

    Simple query with query methods

--- apiRestNoSQL/src/main/java/com/coursejava/apiRestNoSQL/controllers/PostResource.java
+++ apiRestNoSQL/src/main/java/com/coursejava/apiRestNoSQL/controllers/PostResource.java
@@ -1,13 +1,13 @@
 package com.coursejava.apiRestNoSQL.controllers;
 
+import com.coursejava.apiRestNoSQL.controllers.util.URL;
 import com.coursejava.apiRestNoSQL.entities.Post;
 import com.coursejava.apiRestNoSQL.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,4 +22,10 @@ public class PostResource {
         return ResponseEntity.ok().body(post);
     }
 
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> posts = postService.findByTitle(text);
+        return ResponseEntity.ok().body(posts);
+    }
 }
